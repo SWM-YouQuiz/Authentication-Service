17fb34f6e85019a1a1f4ec114fc9b254f956d24c
earlgrey02 <san06036@naver.com>
2023-07-17 14:45:21 +0900

    Refactor: AuthenticationControllerTest 리팩토링

--- src/test/kotlin/com/youquiz/authentication/controller/AuthenticationControllerTest.kt
+++ src/test/kotlin/com/youquiz/authentication/controller/AuthenticationControllerTest.kt
@@ -2,6 +2,7 @@ package com.youquiz.authentication.controller
 
 import com.epages.restdocs.apispec.WebTestClientRestDocumentationWrapper
 import com.ninjasquad.springmockk.MockkBean
+import com.youquiz.authentication.config.SecurityTestConfiguration
 import com.youquiz.authentication.dto.LoginRequest
 import com.youquiz.authentication.dto.LoginResponse
 import com.youquiz.authentication.exception.PasswordNotMatchException
@@ -17,13 +18,14 @@ import com.youquiz.authentication.service.AuthenticationService
 import com.youquiz.authentication.util.BaseControllerTest
 import com.youquiz.authentication.util.desc
 import com.youquiz.authentication.util.errorResponseFields
-import com.youquiz.authentication.util.withMockUser
 import io.mockk.coEvery
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
 import org.springframework.restdocs.operation.preprocess.Preprocessors.*
 import org.springframework.restdocs.payload.PayloadDocumentation.requestFields
 import org.springframework.restdocs.payload.PayloadDocumentation.responseFields
+import org.springframework.test.context.ContextConfiguration
 
+@ContextConfiguration(classes = [SecurityTestConfiguration::class])
 @WebFluxTest(AuthenticationHandler::class, AuthenticationRouter::class)
 class AuthenticationControllerTest : BaseControllerTest() {
     @MockkBean
@@ -43,7 +45,6 @@ class AuthenticationControllerTest : BaseControllerTest() {
         describe("login()은") {
             context("해당 아이디를 가진 유저가 존재하고 비밀번호가 일치하는 경우") {
                 coEvery { authenticationService.login(any()) } returns loginResponse
-                withMockUser()
 
                 it("상태 코드 200과 LoginResponse를 반환한다.") {
                     webClient
@@ -74,7 +75,6 @@ class AuthenticationControllerTest : BaseControllerTest() {
 
             context("해당 아이디를 가진 유저가 존재하고 비밀번호가 일치하지 않는 경우") {
                 coEvery { authenticationService.login(any()) } throws PasswordNotMatchException()
-                withMockUser()
 
                 it("상태 코드 400과 에러를 반환한다.") {
                     webClient
@@ -102,7 +102,6 @@ class AuthenticationControllerTest : BaseControllerTest() {
 
             context("요청으로 주어진 아이디가 존재하지 않는 경우") {
                 coEvery { authenticationService.login(any()) } throws UserNotFoundException()
-                withMockUser()
 
                 it("상태 코드 404와 에러를 반환한다.") {
                     webClient
