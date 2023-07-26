dcfeed8d5ef6fb2826a1cf2aa376423d7bedd12c
imsung <isyou7487@naver.com>
2023-07-27 01:21:15 +0900

    Build: WebClient URL 수정, property 환경변수 주입으로 변경

--- src/main/kotlin/com/youquiz/authentication/adapter/client/UserClient.kt
+++ src/main/kotlin/com/youquiz/authentication/adapter/client/UserClient.kt
@@ -2,6 +2,7 @@ package com.youquiz.authentication.adapter.client
 
 import com.youquiz.authentication.domain.User
 import com.youquiz.authentication.exception.UserNotFoundException
+import com.youquiz.authentication.global.config.userServiceClient
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
@@ -13,7 +14,7 @@ class UserClient(
 ) {
     suspend fun findByUsername(username: String) =
         webClient.get()
-            .uri("http://localhost:8081/{username}", username)
+            .uri("$userServiceClient/user/username/{username}", username)
             .retrieve()
             .onStatus(HttpStatus.NOT_FOUND::equals) { throw UserNotFoundException() }
             .awaitBody<User>()
