918db06f9364135b5439f512c4a3e263234fe350
earlgrey02 <san06036@naver.com>
2023-07-30 14:06:34 +0900

    Feat: 외부 서비스 URL을 프로퍼티로부터 주입받도록 변경

--- src/main/kotlin/com/youquiz/authentication/adapter/client/UserClient.kt
+++ src/main/kotlin/com/youquiz/authentication/adapter/client/UserClient.kt
@@ -10,11 +10,13 @@ import org.springframework.web.reactive.function.client.awaitBody
 
 @Component
 class UserClient(
-    private val webClient: WebClient
+    private val webClient: WebClient,
+    @Value("\${url.service.user}")
+    private val url: String
 ) {
     suspend fun findByUsername(username: String) =
         webClient.get()
-            .uri("$userServiceClient/user/username/{username}", username)
+            .uri("$url/api/user/username/{username}", username)
             .retrieve()
             .onStatus(HttpStatus.NOT_FOUND::equals) { throw UserNotFoundException() }
             .awaitBody<User>()
