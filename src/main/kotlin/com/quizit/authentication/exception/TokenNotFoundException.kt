515623aca52bf7142088ece338ab4c95d6f85bd2
earlgrey02 <san06036@naver.com>
2023-10-23 15:19:51 +0900

    Feat: 모든 예외 클래스들을 data class로 정의

--- src/main/kotlin/com/quizit/authentication/exception/TokenNotFoundException.kt
+++ src/main/kotlin/com/quizit/authentication/exception/TokenNotFoundException.kt
@@ -2,6 +2,6 @@ package com.quizit.authentication.exception
 
 import com.quizit.authentication.global.exception.ServerException
 
-class TokenNotFoundException(
+data class TokenNotFoundException(
     override val message: String = "토큰을 찾을 수 없습니다."
 ) : ServerException(code = 404, message)
