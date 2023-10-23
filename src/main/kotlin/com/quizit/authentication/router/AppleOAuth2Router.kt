d28c4bc1613d15c8d67281c820e84cdeeb898191
earlgrey02 <san06036@naver.com>
2023-10-23 17:57:18 +0900

    Feat: Apple OAuth 구현

---
+++ src/main/kotlin/com/quizit/authentication/router/AppleOAuth2Router.kt
@@ -0,0 +1,19 @@
+package com.quizit.authentication.router
+
+import com.quizit.authentication.global.annotation.Router
+import com.quizit.authentication.handler.AppleOAuth2Handler
+import org.springframework.context.annotation.Bean
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.router
+
+@Router
+class AppleOAuth2Router {
+    @Bean
+    fun oAuth2Routes(handler: AppleOAuth2Handler): RouterFunction<ServerResponse> =
+        router {
+            "/oauth2".nest {
+                POST("/redirect/apple", handler::redirect)
+            }
+        }
+}
