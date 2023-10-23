9a47e2124f9723528a6be5a6ffd7ac3b02f09d7a
earlgrey02 <san06036@naver.com>
2023-10-24 04:41:07 +0900

    Feat: 테스트에 refresh token 변경 사항 반영

--- src/test/kotlin/com/quizit/authentication/service/AuthenticationServiceTest.kt
+++ src/test/kotlin/com/quizit/authentication/service/AuthenticationServiceTest.kt
@@ -3,7 +3,7 @@ package com.quizit.authentication.service
 import com.quizit.authentication.exception.InvalidAccessException
 import com.quizit.authentication.exception.TokenNotFoundException
 import com.quizit.authentication.fixture.ID
-import com.quizit.authentication.fixture.createRefreshRequest
+import com.quizit.authentication.fixture.REFRESH_TOKEN
 import com.quizit.authentication.fixture.createToken
 import com.quizit.authentication.fixture.jwtProvider
 import com.quizit.authentication.repository.TokenRepository
@@ -42,21 +42,21 @@ class AuthenticationServiceTest : BehaviorSpec() {
             }
 
             When("유효한 리프레쉬 토큰으로 로그인 유지를 시도하면") {
-                val result = authenticationService.refresh(createRefreshRequest())
+                val result = authenticationService.refresh(REFRESH_TOKEN)
                     .getResult()
 
                 Then("해당 유저에 대한 액세스 토큰과 리프레쉬 토큰이 발급된다.") {
                     result.expectSubscription()
-                        .assertNext {
-                            jwtProvider.getAuthentication(it.accessToken).id shouldBe ID
-                            jwtProvider.getAuthentication(it.refreshToken).id shouldBe ID
+                        .assertNext { (response, refreshToken) ->
+                            jwtProvider.getAuthentication(response.accessToken).id shouldBe ID
+                            jwtProvider.getAuthentication(refreshToken).id shouldBe ID
                         }
                         .verifyComplete()
                 }
             }
 
             When("유효하지 않은 리프레쉬 토큰으로 로그인 유지를 시도하면") {
-                val result = authenticationService.refresh(createRefreshRequest(refreshToken = "invalid_token"))
+                val result = authenticationService.refresh("invalid_token")
                     .getResult()
 
                 Then("예외가 발생한다.") {
@@ -71,7 +71,7 @@ class AuthenticationServiceTest : BehaviorSpec() {
             every { tokenRepository.findByUserId(any()) } returns empty()
 
             When("리프레쉬 토큰으로 로그인 유지를 시도하면") {
-                val result = authenticationService.refresh(createRefreshRequest())
+                val result = authenticationService.refresh(REFRESH_TOKEN)
                     .getResult()
 
                 Then("예외가 발생한다.") {
