c081389a3972fab2768317413d1937c253f1e03f
earlgrey02 <san06036@naver.com>
2023-10-23 15:21:43 +0900

    Feat: OAuth 로직 수정

--- src/main/kotlin/com/quizit/authentication/domain/OAuth2UserInfo.kt
+++ src/main/kotlin/com/quizit/authentication/domain/OAuth2UserInfo.kt
@@ -7,18 +7,18 @@ import org.springframework.security.oauth2.core.user.OAuth2User
 sealed class OAuth2UserInfo(
     val email: String,
     val provider: Provider,
-    private val name: String,
+    private val name: String?,
     private val attributes: Map<String, *>
 ) : OAuth2User {
     override fun getAttributes(): Map<String, *> = attributes
 
-    override fun getName(): String = name
+    override fun getName(): String? = name
 
     override fun getAuthorities(): List<GrantedAuthority>? = null
 }
 
 class GoogleOAuth2UserInfo(
-    attributes: Map<String, *>
+    private val attributes: Map<String, *>
 ) : OAuth2UserInfo(
     email = attributes["email"] as String,
     provider = Provider.GOOGLE,
@@ -36,7 +36,7 @@ class AppleOAuth2UserInfo(
 )
 
 class KakaoOAuth2UserInfo(
-    attributes: Map<String, *>
+    private val attributes: Map<String, *>
 ) : OAuth2UserInfo(
     email = (attributes["kakao_account"] as Map<*, *>)["email"] as String,
     provider = Provider.KAKAO,
