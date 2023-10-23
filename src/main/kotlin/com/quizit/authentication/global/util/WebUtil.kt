6a70d817bc495fc780adc81411e3d20cc36baca2
earlgrey02 <san06036@naver.com>
2023-10-24 01:35:22 +0900

    Feat: WebUtil에 쿼리 파라미터 관련 메서드 추가

--- src/main/kotlin/com/quizit/authentication/global/util/WebUtil.kt
+++ src/main/kotlin/com/quizit/authentication/global/util/WebUtil.kt
@@ -1,9 +1,24 @@
 package com.quizit.authentication.global.util
 
 import com.github.jwt.authentication.DefaultJwtAuthentication
+import org.springframework.web.reactive.function.server.RequestPredicate
+import org.springframework.web.reactive.function.server.RouterFunctionDsl
 import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.queryParamOrNull
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.cast
+
+fun RouterFunctionDsl.queryParams(vararg names: String): RequestPredicate =
+    names.map { queryParam(it) { true } }.reduce { total, next -> total and next }
+
+inline fun <reified T> ServerRequest.queryParamNotNull(name: String): T =
+    this.queryParamOrNull(name)!!.run {
+        when (T::class) {
+            Int::class -> toInt()
+            Boolean::class -> toBoolean()
+            else -> this
+        } as T
+    }
 
 fun ServerRequest.authentication(): Mono<DefaultJwtAuthentication> =
     principal()
