428be062897ba3f9171fb32570afc22eb1791485
earlgrey02 <san06036@naver.com>
2023-10-23 15:20:58 +0900

    Feat: User 엔티티 변경 사항 적용

--- src/main/kotlin/com/quizit/authentication/adapter/client/UserClient.kt
+++ src/main/kotlin/com/quizit/authentication/adapter/client/UserClient.kt
@@ -1,15 +1,13 @@
 package com.quizit.authentication.adapter.client
 
+import com.quizit.authentication.domain.enum.Provider
 import com.quizit.authentication.dto.request.CreateUserRequest
-import com.quizit.authentication.dto.request.MatchPasswordRequest
-import com.quizit.authentication.dto.response.MatchPasswordResponse
 import com.quizit.authentication.dto.response.UserResponse
-import com.quizit.authentication.exception.OAuthLoginException
+import com.quizit.authentication.exception.UserAlreadyExistException
 import com.quizit.authentication.exception.UserNotFoundException
-import com.quizit.authentication.exception.UsernameAlreadyExistException
+import com.quizit.authentication.global.annotation.Client
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
-import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
 import reactor.core.publisher.Mono
@@ -20,27 +18,18 @@ class UserClient(
     @Value("\${url.service.user}")
     private val url: String
 ) {
-    fun getUserByUsername(username: String): Mono<UserResponse> =
+    fun getUserByUsernameAndProvider(username: String, provider: Provider): Mono<UserResponse> =
         webClient.get()
-            .uri("$url/user/username/{username}", username)
+            .uri("$url/user/username/{username}?provider={provider}", username, provider)
             .retrieve()
             .onStatus(HttpStatus.NOT_FOUND::equals) { Mono.error(UserNotFoundException()) }
             .bodyToMono<UserResponse>()
-
-    fun matchPassword(username: String, request: MatchPasswordRequest): Mono<MatchPasswordResponse> =
-        webClient.post()
-            .uri("$url/user/username/{username}/match-password", username)
-            .bodyValue(request)
-            .retrieve()
-            .onStatus(HttpStatus.NOT_FOUND::equals) { Mono.error(UserNotFoundException()) }
-            .onStatus(HttpStatus.BAD_REQUEST::equals) { Mono.error(OAuthLoginException()) }
-            .bodyToMono<MatchPasswordResponse>()
 
     fun createUser(request: CreateUserRequest): Mono<UserResponse> =
         webClient.post()
             .uri("$url/user")
             .bodyValue(request)
             .retrieve()
-            .onStatus(HttpStatus.CONFLICT::equals) { Mono.error(UsernameAlreadyExistException()) }
+            .onStatus(HttpStatus.CONFLICT::equals) { Mono.error(UserAlreadyExistException()) }
             .bodyToMono<UserResponse>()
 }
