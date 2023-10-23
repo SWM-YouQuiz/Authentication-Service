e5acde3efb0d484f0eb99e8b29dc402ab8c36ab0
earlgrey02 <san06036@naver.com>
2023-10-24 00:32:05 +0900

    Feat: KafkaConfiguration 구현

---
+++ src/main/kotlin/com/quizit/authentication/global/config/KafkaConfiguration.kt
@@ -0,0 +1,36 @@
+package com.quizit.authentication.global.config
+
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate
+import org.springframework.kafka.support.serializer.JsonDeserializer
+import org.springframework.kafka.support.serializer.JsonSerializer
+import reactor.kafka.receiver.ReceiverOptions
+import reactor.kafka.sender.SenderOptions
+
+@Configuration
+class ProducerConfiguration(
+    private val properties: KafkaProperties
+) {
+    @Bean
+    fun reactiveKafkaProducerTemplate(): ReactiveKafkaProducerTemplate<String, Any> =
+        ReactiveKafkaProducerTemplate(
+            SenderOptions.create<String, Any>(properties.buildProducerProperties())
+                .withValueSerializer(JsonSerializer())
+        )
+}
+
+@Configuration
+class ConsumerConfiguration(
+    private val properties: KafkaProperties
+) {
+    private inline fun <reified T> createReceiverOptions(topic: String): ReceiverOptions<String, T> =
+        ReceiverOptions.create<String, T>(
+            properties.run {
+                consumer.groupId = "$topic-group"
+                buildConsumerProperties()
+            })
+            .subscription(listOf(topic))
+            .withValueDeserializer(JsonDeserializer<T>(T::class.java, false))
+}
