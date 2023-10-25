b37e5cbc8f20b49d9924e06654c074ef536af4de
earlgrey02 <san06036@naver.com>
2023-10-26 02:11:34 +0900

    Feat: 테스트 환경에서 ClientRegistration 관련 오류가 발생하던 문제 해결

--- src/test/kotlin/com/quizit/authentication/config/SecurityTestConfiguration.kt
+++ src/test/kotlin/com/quizit/authentication/config/SecurityTestConfiguration.kt
@@ -1,21 +1,45 @@
 package com.quizit.authentication.config
 
 import com.github.jwt.authentication.JwtAuthenticationFilter
+import com.quizit.authentication.domain.enum.Role
 import com.quizit.authentication.fixture.jwtProvider
-import com.quizit.authentication.global.config.SecurityConfiguration
-import com.quizit.authentication.global.oauth.OAuth2LoginSuccessHandler
-import io.mockk.mockk
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
+import org.springframework.http.HttpStatus
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository
 
 @TestConfiguration
 class SecurityTestConfiguration {
     @Bean
-    fun filterChain(http: ServerHttpSecurity): SecurityWebFilterChain =
-        SecurityConfiguration()
-            .filterChain(http, jwtAuthenticationFilter(), mockk<OAuth2LoginSuccessHandler>())
+    fun filterChain(
+        http: ServerHttpSecurity, jwtAuthenticationFilter: JwtAuthenticationFilter
+    ): SecurityWebFilterChain =
+        with(http) {
+            csrf { it.disable() }
+            formLogin { it.disable() }
+            httpBasic { it.disable() }
+            logout { it.disable() }
+            requestCache { it.disable() }
+            securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
+            exceptionHandling { it.authenticationEntryPoint(HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)) }
+            authorizeExchange {
+                it.pathMatchers("/api/auth/admin/**")
+                    .hasAuthority(Role.ADMIN.name)
+                    .pathMatchers(
+                        "/login/**",
+                        "/auth/refresh"
+                    )
+                    .permitAll()
+                    .anyExchange()
+                    .authenticated()
+            }
+            addFilterAt(jwtAuthenticationFilter, SecurityWebFiltersOrder.AUTHORIZATION)
+            build()
+        }
 
     @Bean
     fun jwtAuthenticationFilter(): JwtAuthenticationFilter =
